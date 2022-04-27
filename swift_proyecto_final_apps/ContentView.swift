ee7b0ed61547447958b9915405633b206583d220
Bryan Vargas <bryanvrgsc@gmail.com>
2022-04-27 17:48:01 -0500

    Initial Commit

---
+++ swift_proyecto_final_apps/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift_proyecto_final_apps
+//
+//  Created by Bryan Vargas on 27/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift_proyecto_final_apps/swift_proyecto_final_appsApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_proyecto_final_appsApp.swift
+//  swift_proyecto_final_apps
+//
+//  Created by Bryan Vargas on 27/04/22.
+//
+
+import SwiftUI
+
+@main
+struct swift_proyecto_final_appsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
