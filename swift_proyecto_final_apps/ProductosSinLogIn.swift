bcd14d297e4785a417af68ac89adca80583f6a9d
Bryan Vargas <bryanvrgsc@gmail.com>
2022-05-16 19:42:55 -0500

    padding

--- swift_proyecto_final_apps/ProductosSinLogIn.swift
+++ swift_proyecto_final_apps/ProductosSinLogIn.swift
@@ -32,7 +32,7 @@ struct ProductosSinLogIn: View {
                             )
                         }
                     )
-                }
+                }.padding(.horizontal)
 //                    }
 //                )
                 //                    }
